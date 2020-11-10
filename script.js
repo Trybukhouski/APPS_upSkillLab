4d222a730148acfb022966ea4e32ec67324d8453
Ruslan Trybukhouski <trybukhouskij@gmail.com>
2020-11-10 15:27:46 +0300

    feat: add fotofilters

---
+++ script.js
@@ -0,0 +1,10 @@
+let controls = document.querySelector('.controls');
+let inputs = controls.querySelectorAll('.controls input');
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
