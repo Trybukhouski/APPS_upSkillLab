b2db69a4216bdb5a3902c732974c4efffeffd718
Ruslan Trybukhouski <trybukhouskij@gmail.com>
2020-11-26 19:26:25 +0300

    feat: add timer

--- script.js
+++ script.js
@@ -8,10 +8,14 @@ let scoreArea = document.querySelector('.score');
 let water = document.querySelector('.water');
 let playBtn = document.querySelector('.play');
 let fallenDrop = document.querySelector('.fallen-drop');
+let gameScreen = document.querySelector('.game-screen');
+let header = document.querySelector('.header');
+let countdown = document.querySelector('.countdown');
 
 //Start position
 numberInDrop.innerText = 'Lets play';
 navigationArea.classList.add('navigation-stop');
+fallenDrop.hidden = true;
 
 //Mode Select
 playBtn.addEventListener('click', gameMode);
@@ -23,18 +27,22 @@ function gameMode() {
 
 //Stop Mode
 function stopMode() {
+
   numberInDrop.innerText = 'Lets play';
   navigationArea.classList.add('navigation-stop');
   scoreArea.value = '';
   water.hidden = true;
+  fallenDrop.hidden = true;
 }
 
 
 //Play Mode
 function playMode() {
+  // dropping()
   numberInDrop.innerText = '0';
   navigationArea.classList.remove('navigation-stop')
   water.hidden = false;
+  fallenDrop.hidden = false;
   return result();
 }
 
@@ -61,13 +69,29 @@ enterBtn.addEventListener('click', result);
 
 //Add drop tack
 let lastRes = 0;
-let i = 0;
 let operands = ['-', '+', '/', '*'];
 let scoreLevel = 0;
 let waterLevel = 0;
+let i = 0;
 
 function result() {
+  //Timer
+  let timer;
+  let x = 10;
+  function letCountdown() {
+    countdown.innerHTML = x;
+    x--;
+    if (x < 0) {
+      countdown.innerHTML = 'Time out';
+    }
+    else {
+      timer = setTimeout(letCountdown, 1000);
+    }
+  }
+  letCountdown()
 
+
+  //Task change
   let res = Math.round(Math.random(0, 1) * 100);
   let a = res / 2;
 
@@ -100,17 +124,3 @@ function result() {
   water.style.cssText = `height: ${waterLevel}px`;
 }
 
-// //Time
-// fallenDrop.addEventListener('click', dropping);
-// function dropping() {
-//   let start = Date.now();
-
-//   let timer = setInterval(function () {
-//     let timePassed = Date.now() - start;
-
-//     fallenDrop.style.top = timePassed / 600 + 'px';
-
-//     if (timePassed > 6000) clearInterval(timer);
-
-//   }, 1000);
-// }
