92fb5c79d22455634ec9e75585198475cdcf7d2a
Ruslan Trybukhouski <trybukhouskij@gmail.com>
2020-11-30 21:22:16 +0300

    feat: add background music and other sounds

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let numbers = document.querySelectorAll('.number');
 let displayArea = document.querySelector('.textarea');
 let clearBtn = document.querySelector('.clear');
 let enterBtn = document.querySelector('.enter');
-let numberInDrop = document.querySelector('.drop');
+let task = document.querySelector('.drop');
 let scoreArea = document.querySelector('.score');
 let water = document.querySelector('.water');
 let playBtn = document.querySelector('.play');
@@ -11,52 +11,74 @@ let fallenDrop = document.querySelector('.fallen-drop');
 let gameScreen = document.querySelector('.game-screen');
 let header = document.querySelector('.header');
 let countdown = document.querySelector('.countdown');
+let operationType = document.querySelector('.operation-type');
+let audioAuto = document.querySelector('.backgroundMusic');
+let audioSuccess = document.querySelector('.success');
+let audioFail = document.querySelector('.fail');
 
-//Start position
-numberInDrop.innerText = 'Lets play';
-navigationArea.classList.add('navigation-stop');
-fallenDrop.hidden = true;
-
-//Mode Select
-playBtn.addEventListener('click', gameMode);
-
-function gameMode() {
-  playBtn.innerText === '►' ? playBtn.innerText = '❙❙' : playBtn.innerText = '►';
-  playBtn.innerText === '►' ? stopMode() : playMode();
-}
-
-//Stop Mode
-function stopMode() {
-
-  numberInDrop.innerText = 'Lets play';
-  navigationArea.classList.add('navigation-stop');
-  scoreArea.value = '';
-  water.hidden = true;
-  fallenDrop.hidden = true;
-}
-
-
-//Play Mode
-function playMode() {
-  // dropping()
-  numberInDrop.innerText = '0';
-  navigationArea.classList.remove('navigation-stop')
-  water.hidden = false;
-  fallenDrop.hidden = false;
-  return result();
-}
-
-//Add numbers input
+//Keyboard and Display
 for (let i = 0; i < numbers.length; i++) {
   let number = numbers[i];
   number.addEventListener('click', function (a) {
     display(a.target.textContent);
   });
 };
-
 function display(number) {
   displayArea.value === "0" ? displayArea.value = number : displayArea.value += number;
 };
+
+//Start position
+task.innerText = 'Lets play';
+navigationArea.classList.add('navigation-stop');
+fallenDrop.hidden = true;
+operationType.hidden = true;
+countdown.hidden = true;
+
+
+//Memory
+let lastTaskRes = 0;
+let ErrorReserve = 3;
+let waterLevel = 0;
+let scoreLevel = 0;
+let gameOver = false;
+let indexOfOperandsArr = 0;
+let arithmeticProgressionNumber = 1;
+
+//Mode Select
+playBtn.addEventListener('click', gameMode);
+function gameMode() {
+  playBtn.innerText === '►' ? playBtn.innerText = '❙❙' : playBtn.innerText = '►';
+  playBtn.innerText === '►' ? stopMode() : playMode();
+}
+
+
+//Stop Mode
+function stopMode() {
+  audioAuto.pause();
+  task.innerText = 'Lets play';
+  navigationArea.classList.add('navigation-stop');
+  water.hidden = true;
+  fallenDrop.hidden = true;
+  countdown.hidden = true;
+  operationType.hidden = true;
+  scoreLevel = 0;
+  scoreArea.value = scoreLevel;
+  ErrorReserve = 3;
+}
+
+//Play Mode
+function playMode() {
+  audioAuto.play();
+  task.innerText = '0';
+  navigationArea.classList.remove('navigation-stop')
+  water.hidden = false;
+  fallenDrop.hidden = false;
+  countdown.hidden = false;
+  operationType.hidden = false;
+  // scoreLevel = 0;
+  // scoreArea.value = scoreLevel;
+  return result;
+}
 
 //Add clear button
 clearBtn.addEventListener('click', letClear);
@@ -67,60 +89,103 @@ function letClear() {
 //Add result check
 enterBtn.addEventListener('click', result);
 
-//Add drop tack
-let lastRes = 0;
-let operands = ['-', '+', '/', '*'];
-let scoreLevel = 0;
-let waterLevel = 0;
-let i = 0;
 
 function result() {
-  //Timer
-  let timer;
-  let x = 10;
-  function letCountdown() {
-    countdown.innerHTML = x;
-    x--;
-    if (x < 0) {
-      countdown.innerHTML = 'Time out';
-    }
-    else {
-      timer = setTimeout(letCountdown, 1000);
-    }
+
+  // RANDOM TASK
+
+  let operands = ['-', '+', '/', '*'];
+  function highLevel() {
+    let high = 10;
+    if (scoreLevel <= 0) {
+      high = 10;
+    } else if (scoreLevel <= 10) {
+      high = 100;
+    } else if (scoreLevel <= 20) {
+      high = 1000;
+    } else if (scoreLevel <= 30) {
+      high = 10000;
+    };
+    return high;
   }
-  letCountdown()
-
-
-  //Task change
-  let res = Math.round(Math.random(0, 1) * 100);
-  let a = res / 2;
-
-  i < operands.length - 1 ? i++ : i = 0;
-
-  function taskInDrop() {
-    switch (operands[i]) {
-      case '-': return `${res + a} - ${a}`;
-      case '+': return `${res - a} + ${a}`;
-      case '/': return `${res * a} / ${a}`;
-      case '*': return `${res / a} * ${a}`;
+  let newTaskRes = Math.round(Math.random(0, 1) * highLevel());
+  let a = newTaskRes / 2;
+  indexOfOperandsArr < operands.length - 1 ? indexOfOperandsArr++ : indexOfOperandsArr = 0;
+  function RandomTask() {
+    switch (operands[indexOfOperandsArr]) {
+      case '-': return `${newTaskRes + a} - ${a}`;
+      case '+': return `${newTaskRes - a} + ${a}`;
+      case '/': return `${newTaskRes * a} / ${a}`;
+      case '*': return `${newTaskRes / a} * ${a}`;
     }
   }
 
-  numberInDrop.innerText = taskInDrop();
-  console.log(res);
+  // OPERATION TYPE
+  function showOperationType() {
+    switch (operands[indexOfOperandsArr]) {
+      case '-': return 'subtraction operation:';
+      case '+': return 'addition operation:';
+      case '/': return 'division operation:';
+      case '*': return 'multiplication operation:';
+    }
+  }
 
-  // Score level & Water level
-  if (+displayArea.value - lastRes === 0) {
-    scoreLevel += 1;
+  // STATUS CHECK
+
+  //    Game Over?
+  ErrorReserve === 0 ? gameOver = true : gameOver = false;
+
+  // MAIN SCREEN
+
+  //    Countdown screen
+  gameOver === true ? countdown.innerText = `Result: ${scoreLevel}` : console.log('Play');
+  //    Task screen
+  gameOver === true ? task.innerText = `Game over` : task.innerText = RandomTask();
+  console.log(newTaskRes);
+  //    Operation type
+  gameOver === true ? operationType.innerText = '' : operationType.innerText = showOperationType();
+  //    Navigation
+  gameOver === true ? navigationArea.classList.add('navigation-stop') : navigationArea.classList.remove('navigation-stop');
+
+
+  //    Timer
+
+  // let timer;
+  // let x = 10;
+  // function letCountdown() {
+  //   countdown.innerHTML = x;
+  //   x--;
+  //   if (x < 0) {
+  //     countdown.innerHTML = 'Time out';
+  //   } else if (+displayArea.value - lastTaskRes === 0) {
+  //     countdown.innerHTML = '';
+  //   } else {
+  //     timer = setTimeout(letCountdown, 1000);
+  //   }
+  // }
+  // letCountdown()
+
+  // SCORE AND WATER LEVELS
+  function arithmeticProgression() {
+    arithmeticProgressionNumber += 1;
+    return arithmeticProgressionNumber;
+  }
+  if (+displayArea.value - lastTaskRes === 0) {
+    audioSuccess.play();
+    scoreLevel += arithmeticProgression();
     waterLevel;
+    ErrorReserve = ErrorReserve;
   } else {
+    audioFail.play();
     scoreLevel -= 1;
     waterLevel += 10;
+    ErrorReserve -= 1;
   }
   scoreArea.value = scoreLevel;
   displayArea.value = "0";
-  lastRes = res;
+  lastTaskRes = newTaskRes;
 
   water.style.cssText = `height: ${waterLevel}px`;
+  ;
 }
 
